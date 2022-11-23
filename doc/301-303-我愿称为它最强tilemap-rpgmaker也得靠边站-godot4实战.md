be03e69c5298c803722b6b6c785e073f0df6e80b
godotg <jaysunxiao.gmail.com>
2022-11-23 21:38:08 +0800

    test[nav]: 寻路

---
+++ doc/301-303-我愿称为它最强tilemap-rpgmaker也得靠边站-godot4实战.md
@@ -0,0 +1,13 @@
+# 1. godot 4.x 最新的Tilemap 节点，功能非常强大
+
+- tilemap的由tileset组成，tileset由tile一个个单个图块组成
+
+- 基本上通过熟练使用get_cell、set_cell、world_to_local这些函数就可以解决大多数普通关卡制作需求
+
+- 操作相比3.x极大的简化了，我愿称之为最强tilemap
+
+# 2. tilemap使用
+
+- tileset设置
+
+- patterns设置
---
+++ test/script/Demo9-navgation1.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+@onready var nav_2d : NavigationAgent2D = $Player/NavigationAgent2D
+@onready var player : CharacterBody2D = $Player
+@onready var line2D : Line2D = $Line2D
+
+var speed = 400
+
+func _physics_process(delta):
+	if nav_2d.is_navigation_finished():
+		player.velocity = Vector2.ZERO
+		return
+	
+	var target = nav_2d.get_next_location()
+	var direction = (target - player.position).normalized() * speed
+	
+	player.velocity = direction
+	nav_2d.set_velocity(direction)
+	player.move_and_slide()
+	line2D.points = nav_2d.get_nav_path()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton:
+		return
+		
+	print("aaaaaaaaaaaaaaaaaaaaaaaaa")
+	var playerPosition = player.global_position
+	var mousePosition = event.global_position
+	nav_2d.set_target_location(mousePosition)
+
